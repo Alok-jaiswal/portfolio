11a048f8f24cbf163320297ed5a19214f2674eff
Alok Jaiswal <alok@devbeeit.com>
2024-11-28 12:36:04 +0530

    work on Ui changes

---
+++ src/components/Type.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+function Type() {
+  return (
+    <div>
+      <Typewriter
+        words={[
+          "Software Developer",
+          "Full Stack Developer",
+          "Frontend Developer",
+          "Backend Developer",
+          "Freelancer",
+        ]}
+        loop={0}
+        cursor
+        cursorStyle="|"
+        typeSpeed={70}
+        deleteSpeed={50}
+        delaySpeed={1000}
+      />
+    </div>
+  );
+}
+
+export default Type;
